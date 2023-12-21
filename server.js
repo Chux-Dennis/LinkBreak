e4f3b5c07f25da3188fdbf7a2190988176b16e14
Chux-Dennis <134112520+Chux-Dennis@users.noreply.github.com>
2023-12-21 20:11:45 +0100

    Complete Landing Page

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mo");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const mhash = require("murmurhash-js");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const seed = 0;
 const input = "Dennis256";
 const output = mhash.murmur3(input, seed);
 console.log(input, output);
 
-function trueTest(str, hstr) {
-  const hashedString = mhash.murmur3(str, 0);
-  if (hashedString === hstr) {
-    console.log("MurmurHash is suitable for hashing");
-  } else {
-    console.log("MurmurHash is not suitable for hashing");
-  }
-}
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-trueTest("Dennis256", 1570584766);
-
+app.post("/hero-form", (req, res) => {
+  console.log(req.body);
+});
 let PORT = 3000;
 app.listen(PORT, console.log(`Server is running on ${PORT}`));
